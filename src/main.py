fecc75c399e3136683a68f4603649d93e21903a5
nsp5488 <nsp5488@g.rit.edu>
2024-05-09 22:34:35 -0400

    Basic setup

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    tn = TextNode('test text', 'type', 'google.com')
+    print(tn)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node1 = TextNode("test node", "test")
+        node2 = TextNode("test node", "")
+        self.assertNotEqual(node1, node2)
+
+    def test_repr(self):
+        node1 = TextNode("a", "b", 'test.com')
+        self.assertEqual("TextNode(a, b, test.com)", str(node1))
+
+    @unittest.expectedFailure
+    def test_invalid_constructor(self):
+        _ = TextNode("a")
+        
+
+if __name__ == "__main__":
+    unittest.main()
